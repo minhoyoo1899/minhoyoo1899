f1f347a76ee3e682321647a585d058b0ce8747f3
minhoyoo <minhoyoo1899@gmai.com>
2022-09-03 23:14:29 +0900

    오타수정, css파일 빌드에서 분류

--- project/build/style.js
+++ project/build/style.js
@@ -1,5 +1,5 @@
 export default function divStyle(divName, stArr) {
-  for (let i = 0; i < div.length; i++) {
+  for (let i = 0; i < divName.length; i++) {
     const stObj = stArr[i];
     for (const keyName in stObj) {
       divName[i].style[keyName] = stObj[keyName];
